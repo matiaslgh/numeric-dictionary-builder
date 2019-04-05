1331c48cc8e5337a97e8f151791662f671b91cc3
Matias Luraghi <matiaslgh@gmail.com>
2019-04-05 18:37:27 -0300

    Add coverage support + get 100% cov of args.js

--- args.test.js
+++ args.test.js
@@ -10,6 +10,28 @@ describe('Args class', () => {
   });
 
   describe('args.option()', () => {
+    it('throws an error if no name is provided', () => {
+      expect(() => {
+        args.option();
+      }).toThrowError('A name is required');
+    });
+
+    it('accepts an array of names', () => {
+      const names = ['name_1', 'name_2'];
+      const config = ['a description', 'default value', false];
+      args.option(names, ...config);
+
+      names.forEach((name, index) => {
+        expect(args.opt[index]).toEqual({
+          name: name,
+          description: config[0],
+          default_: config[1],
+          multipleUses: config[2],
+          init: false,
+        });
+      });
+    });
+
     it('adds data into internal array', () => {
       const init = jest.fn();
       args.option('name1', 'description1', 'default1', true, init);

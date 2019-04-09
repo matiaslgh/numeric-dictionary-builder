a127af87c816a26b81f952b0bea2895268102097
Matias Luraghi <matiaslgh@gmail.com>
2019-04-09 12:26:14 -0300

    Create test suite for dictionaryBuilder

---
+++ src/dictionaryBuilder.test.js
@@ -0,0 +1,29 @@
+describe('dictionaryBuilder(...)', () => {
+  it('uses default values if it does not receive params', () => {
+    // TODO: Implement this test
+  });
+
+  it('creates a dictionary from init to end', () => {
+    // TODO: Implement this test
+  });
+
+  it('creates a dictionary with prefixes and suffixes', () => {
+    // TODO: Implement this test
+  });
+
+  it('saves the content in batches', () => {
+    // TODO: Implement this test
+  });
+
+  it('uses default values if it does not receive params', () => {
+    // TODO: Implement this test
+  });
+
+  it('allows to build multiples dictionaries at once in a single file', () => {
+    // TODO: Implement this test
+  });
+
+  it('throws an error when end is less than or equal than init', () => {
+    // TODO: Implement this test
+  });
+});

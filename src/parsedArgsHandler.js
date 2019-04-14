ad8eb1ae0e0e580e420ad6a7f0e591d0b49bda64
Matias Luraghi <matiaslgh@gmail.com>
2019-04-14 11:50:32 -0300

    Add length validation in parsedArgsHandler

--- src/parsedArgsHandler.js
+++ src/parsedArgsHandler.js
@@ -1,5 +1,18 @@
 const dictionaryBuilder = require('./dictionaryBuilder');
+const { INIT, END } = require('./defaults');
+
+const amountErrorMsg = "The amount of 'init' must be equal to the amount of 'end'";
+
+const validate = ({ init = INIT, end = END }) => {
+  const initArr = init instanceof Array ? init : [init];
+  const endArr = end instanceof Array ? end : [end];
+
+  if (initArr.length !== endArr.length) {
+    throw new Error(amountErrorMsg);
+  }
+};
 
 module.exports = function parsedArgsManager(params) {
+  validate(params);
   dictionaryBuilder(params);
 };
--- src/parsedArgsHandler.test.js
+++ src/parsedArgsHandler.test.js
@@ -22,4 +22,29 @@ describe('parsedArgsHandler(...)', () => {
     expect(dictionaryBuilder).toHaveBeenCalledTimes(1);
     expect(dictionaryBuilder).toHaveBeenCalledWith(params);
   });
+
+  it('throws an error when the length of init and end is not the same', () => {
+    const wrongConfigs = [
+      { init: [0, 100], end: [10, 110, 500] },
+      { init: 0, end: [10, 110, 500] },
+      { init: [0, 100], end: 500 },
+      { init: [0, 100] },
+      { end: [0, 100] },
+    ];
+
+    wrongConfigs.forEach(conf => {
+      expect(() => paHandler(conf)).toThrowError(
+        "The amount of 'init' must be equal to the amount of 'end'"
+      );
+    });
+
+    const goodConfigs = [{ init: 2 }, { end: 5 }];
+
+    goodConfigs.forEach(conf => {
+      paHandler(conf);
+      expect(dictionaryBuilder).toHaveBeenLastCalledWith(conf);
+    });
+
+    expect(dictionaryBuilder).toHaveBeenCalledTimes(goodConfigs.length);
+  });
 });

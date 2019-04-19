a9eaa2979784d3a3f682f3f86122507a7d57e42f
Matias Luraghi <matiaslgh@gmail.com>
2019-04-19 10:39:31 -0300

    Document args.js

--- src/args.js
+++ src/args.js
@@ -1,11 +1,27 @@
 const { removeFirstHyphens, isAnOption } = require('./utils');
+
+/**
+ * Class inspired on https://www.npmjs.com/package/args to handle the script arguments.
+ * This one has a lot less features than the original,
+ * but it allows you to use a same argument several times and getting an array:
+ * script -a 1 -a 2 -a 3 --> { a: [1, 2, 3] }
+ */
 class Args {
   constructor() {
     this.options = new Map();
     this.config = {};
   }
 
-  option(name, description = '', default_ = '', init) {
+  /**
+   * The arguments received by the script will be handled by this class only if
+   * they were defined as an option using this method
+   *
+   * @param {string|string[]} name The script can receive args with these names
+   * @param {string} description This will be shown when the help is required
+   * @param {*} default_ Default value
+   * @returns {Object} Returns this to be chainable
+   */
+  option(name, description = '', default_ = '') {
     if (!name) throw new Error('A name is required');
 
     let arrName = name;
@@ -19,7 +35,6 @@ class Args {
     this.options.set(realName, {
       description,
       default_,
-      init,
     });
 
     aliases.forEach(alias => {
@@ -31,11 +46,22 @@ class Args {
     return this;
   }
 
+  /**
+   * Clean the state of the class instance by setting the default values to their properties
+   * Useful for testing.
+   */
   clean() {
     this.options = new Map();
     this.config = {};
   }
 
+  /**
+   * Process process.argv and returns the options names as key with their corresponding values
+   * If the option has multiple names, the object will have the first one as key
+   *
+   * @param {string[]} _params Normally this would be process.argv
+   * @returns {Object} args name as key and their values as value
+   */
   parse(_params) {
     if (!(_params instanceof Array)) throw new Error('Array expected');
 

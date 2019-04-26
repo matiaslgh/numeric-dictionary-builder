760306067cbc7237033a5f35d599979356fbd6bb
Matias Luraghi <matiaslgh@gmail.com>
2019-04-26 12:18:38 -0300

    Add an example to the script

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ args
     ['batchSize', 'b'],
     'Amount of generated keys saved in memory before adding them into the output file',
     defaults.BATCH_SIZE
+  )
+  .example(
+    '-p 014 -p 004 -p 024 -i 170000 -e 999999 -i 1700000 -e 9999999 -i 17000000 -e 99999999',
+    "Generates the file with 014{170k-999999}, 004{170k-999999}, 024{170k-999999}.. And the same for the other 2 ranges (1,7m to 9m and 17m to 99m).. Ideal for fibertel's default wifi password "
   );
 
 parsedArgsHandler(args.parse(process.argv));
